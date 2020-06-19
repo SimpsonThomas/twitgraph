3a6d308f66770ac7eb6d1ee019bba2d15e05c315
Thomas Simpson <simpsonthomas99@gmail.com>
2020-06-19 21:57:07 +0100

    Changed App to a class

--- src/core/App.js
+++ src/core/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Navigation from './navigation/navigation.js'
+import pageList from '../constants/pages'
 
 import {
   BrowserRouter as Router,
@@ -10,29 +11,33 @@ import {
   Link
 } from "react-router-dom";
 
-function routeMaker(item) {
-  // returns routes
-  return(
-      <Route
-          key={item.link}
-          component={item.component}
-          exact path={item.link}
-      />
-  )
-}
 
-function App() {
-  return (
-    <div className="App">
-      
-      <Router>
-        <Navigation></Navigation>
-        <Switch>
-          
-        </Switch>
-      </Router>
-    </div>
-  );
+
+class App extends React.Component {
+  routeMaker(item) {
+    // returns routes
+    return(
+        <Route
+            key={item.link}
+            component={item.component}
+            exact path={item.link}
+        />
+    )
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        
+        <Router>
+          <Navigation></Navigation>
+          <Switch>
+            { pageList.map(page => this.routeMaker(page)) }
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
